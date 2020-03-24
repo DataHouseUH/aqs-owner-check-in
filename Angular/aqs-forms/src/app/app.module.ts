97c96b70ae1cdc4702f3ea81a4428b760f961a7e
christinebramos <34110637+christinebramos@users.noreply.github.com>
2020-03-23 18:20:39 -1000

    Update app.module.ts

--- Angular/aqs-forms/src/app/app.module.ts
+++ Angular/aqs-forms/src/app/app.module.ts
@@ -8,18 +8,30 @@ import { OwnerFormComponent } from './owner-form/owner-form.component';
 import { FrontDisplayComponent } from './front-display/front-display.component';
 import { MatTableModule } from '@angular/material/table';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BackDisplayComponent } from './back-display/back-display.component';
+import { MatCheckboxModule} from '@angular/material/checkbox';
+import { KioskIntialComponent } from './kiosk-intial/kiosk-intial.component';
+import { KioskStartComponent } from './kiosk-start/kiosk-start.component';
+import { SearchScreenComponent } from './search-screen/search-screen.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
     AppComponent,
     OwnerFormComponent,
-    FrontDisplayComponent
+    FrontDisplayComponent,
+    BackDisplayComponent,
+    KioskIntialComponent,
+    KioskStartComponent,
+    SearchScreenComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatTableModule
+    MatTableModule,
+    MatCheckboxModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
